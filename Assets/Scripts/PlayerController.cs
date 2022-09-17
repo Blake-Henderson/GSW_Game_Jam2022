27f8616e925b3f1caf960569ad7e86da86fef440
Brandon Lee <b.lee42000@gmail.com>
2022-09-16 20:59:04 -0500

    Player prefab, health bar, movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D body;
     public SpriteRenderer spriteRenderer;
+    [SerializeField] Slider HealthSlider;
 
     public float health;
 
-    private float movementSpeed;
+    public float movementSpeed;
 
     Vector2 direction;
     // Start is called before the first frame update
@@ -21,6 +23,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        direction = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        body.MovePosition(body.position+(direction * movementSpeed * Time.deltaTime));
+        HealthSlider.value = health;
     }
 }
