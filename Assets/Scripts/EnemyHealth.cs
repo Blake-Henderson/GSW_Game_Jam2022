636b4a248319bf80c0e95d7fa1c5612f3e25a206
Aiden Thacker <aidenthacker86@gmail.com>
2022-09-17 01:18:40 -0500

    Integrating Health System to AI

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public enum element
+    {
+
+    }
+
+    public int maxHealth = 5;
+    public int currentHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
