90ed0a1df24ba80747c84af2552d66e7254e4973
Xander Adams <xanderadams555@gmail.com>
2022-09-18 04:09:37 -0500

    Slot Revision and Aspect Corrections
    Slots now only hold 1 item, know what item they are, holding, and can reference the item they are holding.
    
    All sprites (Except the projectile sprites) now have a 32 pixel per unit size instead of 100.

--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -5,13 +5,23 @@ using UnityEngine.EventSystems;
 
 public class Slot : MonoBehaviour, IDropHandler
 { 
-   public void OnDrop(PointerEventData eventData) 
-   {
-    Debug.Log("OnDrop");
-    if ((eventData.pointerDrag != null) && 
-    (gameObject.GetComponent<ComponentType>().type == eventData.pointerDrag.GetComponent<ComponentType>().type))
-    {
-        eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
-    }
-   }
+       public GameObject contents = null;
+       public void OnDrop(PointerEventData eventData)
+        {
+            Debug.Log("OnDrop");
+            if (((eventData.pointerDrag != null) && 
+            (gameObject.GetComponent<ComponentType>().type == eventData.pointerDrag.GetComponent<ComponentType>().type)) &&
+            contents == null)
+            {
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                contents = eventData.pointerDrag;
+                contents.GetComponent<DragAndDrop>().slot = gameObject.GetComponent<Slot>();
+            }
+        }
+        public void Remove()
+        {
+            contents = null;
+        }
+
+
 }
