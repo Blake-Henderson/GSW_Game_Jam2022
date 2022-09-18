e67835c4d8292294bed18ff2ac05d116bc8c2b8f
Xander Adams <xanderadams555@gmail.com>
2022-09-18 04:54:47 -0500

    Crafted Slot Upgrade
    Crafted slot can now reference other slots

---
+++ Assets/Scripts/CompSlots.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompSlots : MonoBehaviour
+{
+    [SerializeField] private Slot elementSlot;
+    [SerializeField] private Slot attackSlot;
+    [SerializeField] private Slot effectSlot; 
+}
