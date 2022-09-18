eef980810f7e780aab57492d2079d5bf532ca50f
Brandon Lee <b.lee42000@gmail.com>
2022-09-17 19:11:39 -0500

    scrolling between spells should work, spells needed to test

--- Assets/Scripts/Spells/SpellDataManager.cs
+++ Assets/Scripts/Spells/SpellDataManager.cs
@@ -4,9 +4,53 @@ using UnityEngine;
 
 public class SpellDataManager : MonoBehaviour
 {
+    public int selectedSpell = 0;
     /// <summary>
     /// The list of spells the player has
     /// </summary>
     public List<Spell> spells;
-    
+
+
+    void Start()
+    {
+        SelectSpell();
+    }
+
+    void Update()
+    {
+        int previousSelectedSpell = selectedSpell;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (selectedSpell >= 2)
+                selectedSpell = 0;
+            else
+            selectedSpell++;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            if (selectedSpell <= 0)
+                selectedSpell = 2;
+            else
+                selectedSpell--;
+        }
+        if (previousSelectedSpell != selectedSpell)
+        {
+            SelectSpell();
+        }
+    }
+
+    void SelectSpell()
+    {
+        int i = 0;
+        foreach (Transform spell in transform)
+        {
+            if (i == selectedSpell)
+                spell.gameObject.SetActive(true);
+            else
+                spell.gameObject.SetActive(false);
+            i++;
+        }
+    }
+
 }
