ecadf8748e3d6fd63427aaa6237a383307b02fb3
Blake-Henderson <bmhenderson101@gmail.com>
2022-09-17 22:23:32 -0500

    Spell tweaks

--- Assets/Scripts/Spells/Effects/AOE.cs
+++ Assets/Scripts/Spells/Effects/AOE.cs
@@ -20,4 +20,49 @@ public class AOE : MonoBehaviour
      * see if life steal
      * heal enemies
      */
+
+    public bool stun;
+    public bool slow;
+    public bool dot;
+    public bool lifeSteal;
+    public int damageMultipler = 1;
+    public ParticleSystem particles;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (dot)
+        {
+            if(collision.gameObject.GetComponent<DOT>() == null)
+            {
+                collision.gameObject.AddComponent<DOT>();
+                collision.gameObject.GetComponent<DOT>().lifeSteal = lifeSteal;
+                collision.gameObject.GetComponent<DOT>().damageMultiplier = damageMultipler;
+            }
+        }
+        if(slow)
+        {
+            if (collision.gameObject.GetComponent<Slow>() == null)
+            {
+                collision.gameObject.AddComponent<Slow>();
+            }
+        }
+        if (stun)
+        {
+            if (collision.gameObject.GetComponent<Stun>() == null)
+            {
+                collision.gameObject.AddComponent<Stun>();
+            }
+        }
+        if (lifeSteal)
+        {
+
+        }
+    }
+    private void Update()
+    {
+        if (!particles.isEmitting)
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/Spells/Effects/Stun.cs
+++ Assets/Scripts/Spells/Effects/Stun.cs
@@ -15,4 +15,33 @@ public class Stun : MonoBehaviour
     //{
     //    gameObject.GetComponent<SpriteRenderer>().color = colors[((int)gameObject.GetComponent<SpellDataManager>().spells[0].type)];
     //}
+
+    public float stunDuration = 1;
+    private float stunTimer = 0;
+    private float immuneDuration = 1;
+    private EnemyAI AI;
+    private void Start()
+    {
+        stunTimer = 0.0f;
+        gameObject.TryGetComponent<EnemyAI>(out AI);
+        if(AI == null)
+            AI.enabled = false;
+        else
+        {
+            Destroy(gameObject.GetComponent<Stun>());
+        }
+    }
+    private void Update()
+    {
+        stunTimer += Time.deltaTime;
+        if (stunTimer >= stunDuration + immuneDuration)
+        {
+            Destroy(gameObject.GetComponent<Stun>());
+        }
+        else if (stunTimer >= stunDuration)
+        {
+            AI.enabled = true;
+            
+        }
+    }
 }
