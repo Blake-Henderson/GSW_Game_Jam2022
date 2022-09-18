2fe690d60a37640873d27bc8253a6aecb28e6123
Xander Adams <xanderadams555@gmail.com>
2022-09-18 16:27:09 -0500

    Rush Job
    Big Time work (Hotbar)

--- Assets/Scripts/HotbarAdd.cs
+++ Assets/Scripts/HotbarAdd.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class HotbarAdd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<GameObject> slots;
+    public SpellDataManager spells;
+    public void Update()
+        {
+            for(int i = 0; i<slots.Count; i++)
+            {
+                if(slots[i].GetComponent<Slot>().contents != null)
+                {
+                    Spell temp = slots[i].GetComponent<Slot>().contents.GetComponent<SpellHolder>().spell;
+                    Debug.Log(i);
+                    spells.spells[i]= temp;
+                }
+            }
+        }
 }
